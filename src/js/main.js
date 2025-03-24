24ce06ef2e6e186f6d376de1a7a7123bb8b59bba
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-24 22:04:12 +0300

    fix: specify GET method for API request in form submission

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,7 @@ form.addEventListener('submit', event => {
   const searchValue = form.elements.search.value;
 
   fetch('https://pixabay.com/api/', {
+    method: 'GET',
     body: JSON.stringify({
       key: '21250106-0015933422f1e636de5f184b8',
       q: searchValue,
