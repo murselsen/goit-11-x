b095eb7cf19b55d20b563b63d4821e8b9766bc75
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-24 22:08:19 +0300

    fix: update API request to use URLSearchParams and include Content-Type header

--- src/js/main.js
+++ src/js/main.js
@@ -4,16 +4,19 @@ form.addEventListener('submit', event => {
   event.preventDefault(); // Sayfa Yenilenmesini Engeller
   const searchValue = form.elements.search.value;
 
-  fetch('https://pixabay.com/api/', {
+
+  const params = new URLSearchParams({
+    userId: 123,
+    status: 'active',
+  });
+
+  fetch(`https://pixabay.com/api/?key=21250106-0015933422f1e636de5f184b8&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true`, {
+    headers: {
+      'Content-Type': 'application/json',
+    },
     method: 'GET',
-    body: JSON.stringify({
-      key: '21250106-0015933422f1e636de5f184b8',
-      q: searchValue,
-      image_type: 'photo',
-      orientation: 'horizontal',
-      safesearch: true,
-    }),
   })
+    .then(response => response.json())
     .then(response => {
       console.log(response);
     })
