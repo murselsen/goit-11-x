1deaff4a297ba64aea14014d24445ce24427aa8b
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-24 22:13:05 +0300

    fix: replace fetch with axios for API request and remove unused URLSearchParams

--- src/js/main.js
+++ src/js/main.js
@@ -1,26 +1,16 @@
+import axios from 'axios';
+
 const form = document.querySelector('.app-form');
 
 form.addEventListener('submit', event => {
   event.preventDefault(); // Sayfa Yenilenmesini Engeller
   const searchValue = form.elements.search.value;
 
-
-  const params = new URLSearchParams({
-    userId: 123,
-    status: 'active',
+  axios.get('https://pixabay.com/api/', {
+    params: {
+      key : 
+    }
   });
 
-  fetch(`https://pixabay.com/api/?key=21250106-0015933422f1e636de5f184b8&q=${searchValue}&image_type=photo&orientation=horizontal&safesearch=true`, {
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    method: 'GET',
-  })
-    .then(response => response.json())
-    .then(response => {
-      console.log(response);
-    })
-    .catch(error => {
-      console.error(error);
-    });
+
 });
