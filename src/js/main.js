551fed704f7bd68a36c862758f630f370df8e0e8
Mürsel SEN <murselsen_35@hotmail.com>
2025-03-24 22:02:11 +0300

    fix: add error handling and log response for API request in form submission

--- src/js/main.js
+++ src/js/main.js
@@ -7,7 +7,16 @@ form.addEventListener('submit', event => {
   fetch('https://pixabay.com/api/', {
     body: JSON.stringify({
       key: '21250106-0015933422f1e636de5f184b8',
-      
+      q: searchValue,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
     }),
-  });
+  })
+    .then(response => {
+      console.log(response);
+    })
+    .catch(error => {
+      console.error(error);
+    });
 });
